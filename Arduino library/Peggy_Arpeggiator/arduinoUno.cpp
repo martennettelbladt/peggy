6a4b4417d1e9582222b0ea888e0d9b8130ee509f
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-01 19:47:00 +0100

    Update arduinoUno.cpp

--- Arduino library/Peggy_Arpeggiator/arduinoUno.cpp
+++ Arduino library/Peggy_Arpeggiator/arduinoUno.cpp
@@ -17,6 +17,7 @@
 
 #include "interface.h"
 
+byte midiData;
 byte midiClock = 0xf8;
 
 // -------------------------------- I N T E R F A C E   B E G I N
@@ -54,12 +55,9 @@ void interfaceSyncOut()
 // -------------------------------- I N T E R F A C E   N O T E   O N
 void interfaceNoteOn(byte channel, byte note, byte velocity)
 {
-  if (peggy[0].mute == false)
-  {
-    serialNoteOut.write(0x90 + channel);
-    serialNoteOut.write(note);
-    serialNoteOut.write(velocity);
-  }
+  serialNoteOut.write(0x90 + channel);
+  serialNoteOut.write(note);
+  serialNoteOut.write(velocity);
 }
 
 // -------------------------------- I N T E R F A C E   N O T E   O F F
