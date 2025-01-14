c97d725c73c64e59e8d0e86babb110da992eb9ac
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-14 16:13:21 +0100

    Create scales462.cpp

---
+++ scales462.cpp
@@ -0,0 +1,19 @@
+/*
+A sorted list of the 462 possible scales that divide the octave into 7 intervals.
+
+462 = (11 * 10 * 9 * 8 * 7 * 6) / (6 * 5 * 4 * 3 * 2 * 1) = 332640 / 720
+462 = 66 * 7 (66 unique scales with 7 different modes or 'rotations')
+
+Start of scale list
+
+001: 1, 1, 1, 1, 1, 1, 6
+002: 1, 1, 1, 1, 1, 2, 5
+003: 1, 1, 1, 1, 1, 3, 4
+004: 1, 1, 1, 1, 2, 2, 4
+005: 1, 1, 1, 1, 2, 3, 3
+006: 1, 1, 1, 2, 2, 2, 3
+007: 1, 1, 2, 2, 2, 2, 2
+(...)
+462: 6, 1, 1, 1, 1, 1, 1
+
+*/
