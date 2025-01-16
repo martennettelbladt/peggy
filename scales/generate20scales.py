d69421040cc124805f367a5490198049db912f8c
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-16 14:53:47 +0100

    Update generate20scales.py

--- scales/generate20scales.py
+++ scales/generate20scales.py
@@ -11,7 +11,7 @@ for a in range(3):
                                 # no adjacent "1" except 4th / 5th interval and first / last. Pure 5th
                                 if a + b > 0 and b + c > 0 and c + d > 0 and e + f > 0 and f + g > 0 and a + b + c + d + 4 == 7:
                                     # Select only major scales
-                                    if a + b == 2:
+                                    if a + b + 2 == 4:
                                         # print major / minor scale
                                         print(a+1, b+1, c+1, d+1, "-", e+1, f+1, g+1, " / ", d+1, c+1, b+1, a+1, "-", g+1, f+1, e+1, sep='')
 
