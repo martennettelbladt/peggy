b1bdf22689523e2f15dcb35e45f231e877ffb178
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-17 12:40:43 +0100

    Update generate20x2scales.py

--- scales/generate20x2scales.py
+++ scales/generate20x2scales.py
@@ -1,22 +1,21 @@
 # 20x2 scales / Mårten Nettelbladt / 2025-01-17
 
-for a in range(3):
-    for b in range(3):
-        for c in range(3):
-            for d in range(3):
-                for e in range(3):
-                    for f in range(3):
-                        for g in range(3):
+# range is 1-3
+for a in range(1, 4):
+    for b in range(1, 4):
+        for c in range(1, 4):
+            for d in range(1, 4):
+                for e in range(1, 4):
+                    for f in range(1, 4):
+                        for g in range(1, 4):
                             # Sum of all 7 intervals = 12
-                            if a+1 + b+1 + c+1 + d+1 + e+1 + f+1 + g+1 == 12:
-                                # no adjacent "1" except 4th / 5th interval (and first / last)
-                                if a+1 + b+1 > 2 and b+1 + c+1 > 2 and c+1 + d+1 > 2 and e+1 + f+1 > 2 and f+1 + g+1 > 2:
-                                    # Pure 5th, sum of first 4 intervals = 7
-                                    if a+1 + b+1 + c+1 + d+1 == 7:
-                                        # Select only major scales, sum of first 2 intervals = 4
-                                        if a+1 + b+1 == 4:
+                            if a + b + c + d + e + f + g == 12:
+                                # Pure 5th, sum of first 4 intervals = 7
+                                if a + b + c + d == 7:
+                                    # no adjacent "1" except 4th / 5th interval (and first / last)
+                                    if a + b > 2 and b + c > 2 and c + d > 2 and e + f > 2 and f + g > 2:
+                                        # Select only major scales, sum of first 2 intervals = 4 (major 3rd)
+                                        if a + b == 4:
                                             # print major / minor scale
-                                            print(a+1, b+1, c+1, d+1, "-", e+1, f+1, g+1, " (", d+1, c+1, b+1, a+1, "-", g+1, f+1, e+1, ")", sep='')
-
-                                            # +1 added to every note because range is 0-2 rather than 1-3
+                                            print(a, b, c, d, "-", e, f, g, " (", d, c, b, a, "-", g, f, e, ")", sep='')
 
