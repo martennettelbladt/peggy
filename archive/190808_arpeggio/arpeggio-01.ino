cebb7e7f9c234863e4ddc259e5db0aaace6ad667
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-20 23:11:20 +0100

    Update arpeggio-01.ino

--- archive/190808_arpeggio/arpeggio-01.ino
+++ archive/190808_arpeggio/arpeggio-01.ino
@@ -1,24 +1,4 @@
-/*
-  MIDI note player
-
-  This sketch shows how to use the serial transmit pin (pin 1) to send MIDI note data.
-  If this circuit is connected to a MIDI synth, it will play the notes
-  F#-0 (0x1E) to F#-5 (0x5A) in sequence.
-
-  The circuit:
-  - digital in 1 connected to MIDI jack pin 5
-  - MIDI jack pin 2 connected to ground
-  - MIDI jack pin 4 connected to +5V through 220 ohm resistor
-  - Attach a MIDI cable to the jack, then to a MIDI synth, and play music.
-
-  created 13 Jun 2006
-  modified 13 Aug 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Midi
-*/
+// Mårten Nettelbladt / 2019-08-08
 
 void setup() {
   // Set MIDI baud rate:
