968656ba25cb80da00150998a767972b466bc5d2
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-27 14:04:13 +0100

    Create generate16x2scales.py

---
+++ scales/generate16x2scales.py
@@ -0,0 +1,22 @@
+# 16x2 scales / Mårten Nettelbladt / 2025-01-27
+
+# range is 1-3
+for a in range(1, 4):
+    for b in range(1, 4):
+        for c in range(1, 4):
+            for d in range(1, 4):
+                for e in range(1, 4):
+                    for f in range(1, 4):
+                        for g in range(1, 4):
+                            # Sum of all 7 intervals = 12
+                            if a + b + c + d + e + f + g == 12:
+                                # Pure 5th, sum of first 4 intervals = 7
+                                if a + b + c + d == 7:
+                                    # no adjacent "1" except 4th / 5th interval (and first / last)
+                                    if a + b > 2 and b + c > 2 and c + d > 2 and e + f > 2 and f + g > 2:
+                                        # Select only major scales, sum of first 2 intervals = 4 (major 3rd)
+                                        if a + b == 4:
+                                          # must not have minor third as first interval
+                                          if a != 3:
+                                            # print major / minor scale
+                                            print(a, b, c, d, "-", e, f, g, " (", d, c, b, a, "-", g, f, e, ")", sep='')
